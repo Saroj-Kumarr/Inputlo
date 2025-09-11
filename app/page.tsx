0aafc41e15d924e3f8031e2f421eb7090e8e399b
Saroj-Kumarr <sarojmali6090@gmail.com>
2025-09-11 19:20:11 +0530

    pushed

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const page = () => {
       <FormBuilderLanding />
       <WhyFormsSection />
       <EndlessPossibilities />
-      <IntegrationsMinimal />
+      {/* <IntegrationsMinimal /> */}
       {/* <PricingComparison /> */}
       <StatsCta />
       <TestimonialsSection />
